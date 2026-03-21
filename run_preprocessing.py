b117df2e2be24552733fcf66796bb0d1429e8044
osalodion <uwidiaosalodion@gmail.com>
2026-03-21 22:12:03 +0100

    fixed save_dir error for downloading AFSet and PDBSet files

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -1,6 +1,7 @@
 #Import libraries
 from pathlib import Path
 import preprocessing
+from parser import get_protein_info
 
 #Save paths for AFSet and PDBset
 af_train_data_path = Path("dataset/nrAF-Model-GO_train_sequences.fasta")
@@ -11,31 +12,32 @@ pdb_train_data_path = Path("dataset/nrPDB-GO_2019.06.18_train_sequences.fasta")
 pdb_test_data_path = Path("dataset/nrPDB-GO_2019.06.18_test_sequences.fasta")
 pdb_val_data_path = Path("dataset/nrPDB-GO_2019.06.18_val_sequences.fasta")
 
-#Create Directory for saving retrieved cif files 
-def create_cif_file_download_path(dataset_type, split_name): 
-    return Path(f"structures/{dataset_type}/{split_name}")
-
 
 datasets = [
-    ("pdb", "train", pdb_train_data_path, preprocessing._download_one_pdb_structure),
-    ("pdb", "test", pdb_test_data_path, preprocessing._download_one_pdb_structure),
-    ("pdb", "val", pdb_val_data_path, preprocessing._download_one_pdb_structure),
-    ("af", "train", af_train_data_path, preprocessing._download_one_af_structure),
-    ("af", "test", af_test_data_path, preprocessing._download_one_af_structure),
-    ("af", "val", af_val_data_path, preprocessing._download_one_af_structure),
+    ("pdb", "train", pdb_train_data_path),
+    ("pdb", "test", pdb_test_data_path),
+    ("pdb", "val", pdb_val_data_path),
+    ("af", "train", af_train_data_path),
+    ("af", "test", af_test_data_path),
+    ("af", "val", af_val_data_path),
 ]
 
 pipeline_results = {}
 
-for dataset_type, split, fasta_path, download_fn in datasets:
+for dataset_type, split, fasta_path in datasets:
     if dataset_type not in pipeline_results:
         pipeline_results[dataset_type] = {}
+
 
     pipeline_results[dataset_type][split] = {}
 
     #Download AFSet and PDBset files
-    save_dir = create_cif_file_download_path(dataset_type, f"{dataset_type}_{split}")
+    save_dir = f"structures/{dataset_type}/{dataset_type}_{split}"
 
+    download_fn = preprocessing.download_one_af_structure if dataset_type == "af" else preprocessing.download_one_pdb_structure
+
+
+    print(f"Downloading structures for {dataset_type}_{split}....")
     download_result = preprocessing.download_multiple_structures_fast(
         fasta_path, save_dir, download_fn
     )
@@ -70,18 +72,33 @@ for dataset_type, split, fasta_path, download_fn in datasets:
         split_xray_ids_path = Path(f"retained_xray_ids_pdb_{split}.txt")
         split_fasta_path = Path(f"dataset/nrPDB-GO_2019.06.18_{split}_sequences.fasta")
 
-        fasta_output_name = f"filtered_{split}"
+        output_filename = f"filtered_pdb_{split}"
 
         preprocessing.filter_xray_struct(
             split_xray_ids_path,
             split_fasta_path,
-            fasta_output_name
+            output_filename
         )
 
         pipeline_results[dataset_type][split]["xray_fasta_sequence_filter"] = {
             "input_fasta": str(split_fasta_path),
             "xray_ids": str(split_xray_ids_path),
-            "output_name": fasta_output_name
+            "output_name": output_filename
         }
 
+
+    #Handle AFset specific concerns
+    if dataset_type == "af":
+
+        split_fasta_path = Path(f"dataset/nrAF-Model-GO_{split}_sequences.fasta")
+        output_filename = f"cleaned_af_{split}"
+        preprocessing.clean_af_fasta_file_header(
+            split_fasta_path,
+            output_filename
+        )
+
+        pipeline_results[dataset_type][split]["clean_af_fasta_file_header"] = {
+            "input_fasta": str(split_fasta_path),
+            "output_name": output_filename
+        }
 
