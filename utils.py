eca84d1f285846587b9b73802ab47f4cd26c38fe
osalodion <uwidiaosalodion@gmail.com>
2026-03-31 21:48:24 +0100

    Modified setup_logging() function

--- utils.py
+++ utils.py
@@ -1,14 +1,24 @@
 import logging
 
 def setup_logging():
-    handlers = [logging.StreamHandler()]            
-    handlers.append(logging.FileHandler("pipeline.log", mode="a")) 
+    logger = logging.getLogger()  # root logger
+    
+    # Avoid adding duplicate handlers if called multiple times
+    if logger.handlers:
+        return logger
+    
+    handlers = [
+        logging.StreamHandler(),
+        logging.FileHandler("pipeline.log", mode="a")
+    ]
 
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(message)s",
+        format="%(asctime)s | %(levelname)s | %(name)s | %(funcName)s:%(lineno)d | %(message)s",
         handlers=handlers
     )
+    
+    return logger
 
 logger = logging.getLogger(__name__)
 logger.info("Logger is ready for console and file!")
